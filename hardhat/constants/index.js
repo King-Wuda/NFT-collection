b511e6a763418f89abdbf06ce8dd46db3d642367
Tumelo <mashabaindustriesllc@gmail.com>
2022-07-02 16:32:33 +0200

    initial commit

---
+++ hardhat/constants/index.js
@@ -0,0 +1,5 @@
+const WHITELIST_CONTRACT_ADDRESS = "0xe7b46cF97692Cb8AF42A9F41d862D6CeAfCF1668";
+// URL to extract Metadata for a Crypto Dev NFT
+const METADATA_URL = "https://nft-collection-sneh1999.vercel.app/api/";
+
+module.exports = { WHITELIST_CONTRACT_ADDRESS, METADATA_URL };
